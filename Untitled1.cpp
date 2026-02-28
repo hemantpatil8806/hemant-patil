c37948eeabd959f101fe47aa4c12aebdb55fba0f
hemantpatil8806 <hemantp21052007@gmail.com>
2026-02-28 11:10:45 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+   int num1,num2,multi,sub;
+   
+   printf("enter two integers:");
+   scanf("%d %d",&num1,&num2);
+   //calculate the multi
+   
+   multi=num1*num2;
+   sub=num1-num2;
+   
+   printf("%d*%d=%d",num1,num2,multi);
+   pr
+   
+   intf("%d-%d=%d",num1,num2,sub);
+   
+   return 0;	
+}
